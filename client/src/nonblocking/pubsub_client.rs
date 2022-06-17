47315d44ae6ee59f3b4b1ff1dc5275fddcd4dea9
aimeedeer <aimeez@pm.me>
2022-06-17 15:51:29 -0600

    account_subscribe

--- client/src/nonblocking/pubsub_client.rs
+++ client/src/nonblocking/pubsub_client.rs
@@ -109,7 +109,7 @@ impl PubsubClient {
         })
     }
 
-        /// # Examples
+    /// # Examples
     ///
     /// ```
     /// # use anyhow::Result;
@@ -151,6 +151,92 @@ impl PubsubClient {
         ))
     }
 
+    /// # Examples
+    ///
+    /// ```
+    /// # use anyhow;
+    /// # use futures_util::StreamExt;
+    /// # use solana_account_decoder::UiAccount;
+    /// # use solana_client::{
+    /// #     nonblocking::pubsub_client::{PubsubClient, PubsubClientResult},
+    /// #     rpc_config::RpcAccountInfoConfig,
+    /// #     rpc_response::Response,
+    /// # };
+    /// # use solana_sdk::{
+    /// #     commitment_config::CommitmentConfig,
+    /// #     pubkey::Pubkey,
+    /// # };
+    /// # use tokio::{
+    /// #     runtime::Runtime,
+    /// #     sync::{
+    /// #         mpsc::unbounded_channel,
+    /// #         oneshot,
+    /// #     },
+    /// #     task,
+    /// #     time::{sleep, Duration},
+    /// # };
+    /// # use futures::future::BoxFuture;
+    /// # use futures::stream::BoxStream;
+    /// async fn get_account_updates(account_pubkey: Pubkey) -> anyhow::Result<()> {
+    ///     let (account_sender, mut account_receiver) = unbounded_channel::<Response<UiAccount>>();
+    ///     let (account_unsubscribe_sender, mut account_unsubscribe_receiver) = oneshot::channel();
+    ///
+    ///     let url = "wss://api.devnet.solana.com/";
+    ///     let pubsub_client = PubsubClient::new(url).await.unwrap();
+    ///
+    ///     let task_account_subscribe = tokio::spawn(async move {
+    ///         let (mut account_notifications, account_unsubscribe) = pubsub_client
+    ///             .account_subscribe(
+    ///                 &account_pubkey,
+    ///                 Some(RpcAccountInfoConfig {
+    ///                     commitment: Some(CommitmentConfig::confirmed()),
+    ///                     ..RpcAccountInfoConfig::default()
+    ///                 }),
+    ///             )
+    ///             .await
+    ///             .unwrap();
+    ///
+    ///         if let Err(_) = account_unsubscribe_sender.send(account_unsubscribe) {
+    ///             println!("account_unsubscribe receiver dropped");
+    ///         }
+    ///
+    ///         while let Some(account) = account_notifications.next().await {
+    ///             account_sender.send(account).unwrap();
+    ///         }
+    ///     });
+    ///
+    ///     let task_account_receiver = task::spawn(async move {
+    ///         loop {
+    ///             if let Some(result) = account_receiver.recv().await {
+    ///                 println!("account pubsub result: {:?}", result);
+    ///             } else {
+    ///                 break;
+    ///             }
+    ///         }
+    ///     });
+    ///
+    ///     match account_unsubscribe_receiver.await {
+    ///         Ok(account_unsubscribe) => {
+    ///             sleep(Duration::from_secs(5)).await;
+    ///             account_unsubscribe().await;
+    ///         }
+    ///         Err(e) => {
+    ///             println!("account_unsubscribe_receiver error: {}", e);
+    ///         }
+    ///     }
+    ///
+    ///     task_account_subscribe.await;
+    ///     task_account_receiver.await;
+    ///
+    ///     Ok(())
+    /// }
+    /// #
+    /// # let rt = Runtime::new()?;
+    /// # rt.block_on(async {
+    /// #     get_account_updates(solana_sdk::pubkey::new_rand());
+    /// # });
+    /// # Ok::<(), anyhow::Error>(())
+    /// ```
     pub async fn account_subscribe(
         &self,
         pubkey: &Pubkey,

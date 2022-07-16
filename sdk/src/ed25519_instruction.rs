df6a604eb5f42eae17cb7ea1ecb0d9e941a862c3
aimeedeer <aimeez@pm.me>
2022-07-15 18:34:51 -0600

    onchain program

--- sdk/src/ed25519_instruction.rs
+++ sdk/src/ed25519_instruction.rs
@@ -44,23 +44,6 @@ pub struct Ed25519SignatureOffsets {
 /// #    system_program, sysvar,
 /// # };
 /// #
-/// // shared library between on-chain program and the client program
-///
-/// /// # Accounts
-/// ///
-/// /// - 0: instructions sysvar
-/// #[derive(BorshSerialize, BorshDeserialize, Debug)]
-/// pub struct CustomEd25519Instruction;
-///
-/// impl CustomEd25519Instruction {
-///     pub fn build_instruction(self, program_id: &Pubkey) -> Instruction {
-///         let instr = self;
-///         let accounts = vec![AccountMeta::new_readonly(sysvar::instructions::ID, false)];
-///
-///         Instruction::new_with_borsh(*program_id, &instr, accounts)
-///     }
-/// }
-///
 /// // the client program
 ///
 /// use ed25519_dalek::{Keypair as Ed25519Keypair, Signer as Ed25519Signer, KEYPAIR_LENGTH};
@@ -96,6 +79,23 @@ pub struct Ed25519SignatureOffsets {
 ///
 ///    Ok(())
 /// }
+/// 
+/// // shared library between on-chain program and the client program
+///
+/// /// # Accounts
+/// ///
+/// /// - 0: instructions sysvar
+/// #[derive(BorshSerialize, BorshDeserialize, Debug)]
+/// pub struct CustomEd25519Instruction;
+///
+/// impl CustomEd25519Instruction {
+///     pub fn build_instruction(self, program_id: &Pubkey) -> Instruction {
+///         let instr = self;
+///         let accounts = vec![AccountMeta::new_readonly(sysvar::instructions::ID, false)];
+///
+///         Instruction::new_with_borsh(*program_id, &instr, accounts)
+///     }
+/// }
 /// #
 /// # let client = RpcClient::new(String::new());
 /// # let program_id = Pubkey::new_unique();
@@ -106,6 +106,147 @@ pub struct Ed25519SignatureOffsets {
 /// #     &payer,
 /// # );
 /// # Ok::<(), anyhow::Error>(())
+/// ```
+///
+/// In the on-chain program:
+///
+/// ```
+/// # use solana_program::{
+/// #     account_info::{next_account_info, AccountInfo},
+/// #     ed25519_program,
+/// #     entrypoint::ProgramResult,
+/// #     msg,
+/// #     program_error::ProgramError,
+/// #     sysvar,
+/// # };
+/// use ed25519_dalek::PUBLIC_KEY_LENGTH;
+///
+/// mod ed25519_defs {
+///     use solana_program::program_error::ProgramError;
+///
+///     pub const PUBKEY_SERIALIZED_SIZE: usize = 32;
+///     pub const SIGNATURE_SERIALIZED_SIZE: usize = 64;
+///     pub const SIGNATURE_OFFSETS_SERIALIZED_SIZE: usize = 14;
+///     pub const SIGNATURE_OFFSETS_START: usize = 2;
+///     pub const DATA_START: usize = SIGNATURE_OFFSETS_SERIALIZED_SIZE + SIGNATURE_OFFSETS_START;
+///
+///     #[derive(Default, Debug, Copy, Clone)]
+///     pub struct Ed25519SignatureOffsets {
+///         pub signature_offset: u16, // offset to ed25519 signature of 64 bytes
+///         pub signature_instruction_index: u16, // instruction index to find signature
+///         pub public_key_offset: u16, // offset to public key of 32 bytes
+///         pub public_key_instruction_index: u16, // instruction index to find public key
+///         pub message_data_offset: u16, // offset to start of message data
+///         pub message_data_size: u16, // size of message data
+///         pub message_instruction_index: u16, // index of instruction data to get message data
+///     }
+///
+///     pub fn load_signature_offsets(
+///         ed25519_instr_data: &[u8],
+///     ) -> Result<Ed25519SignatureOffsets, ProgramError> {
+///         // First element is the number of num_signatures
+///         let num_signature = *ed25519_instr_data
+///             .get(0)
+///             .ok_or(ProgramError::InvalidArgument)?;
+///
+///         let instr_data_slice = ed25519_instr_data
+///             .get(2..)
+///             .ok_or(ProgramError::InvalidArgument)?;
+///
+///         fn decode_u16(chunk: &[u8], index: usize) -> u16 {
+///             u16::from_le_bytes(<[u8; 2]>::try_from(&chunk[index..index + 2]).unwrap())
+///         }
+///
+///         let offsets = Ed25519SignatureOffsets {
+///             signature_offset: decode_u16(instr_data_slice, 0),
+///             signature_instruction_index: decode_u16(instr_data_slice, 2),
+///             public_key_offset: decode_u16(instr_data_slice, 4),
+///             public_key_instruction_index: decode_u16(instr_data_slice, 6),
+///             message_data_offset: decode_u16(instr_data_slice, 8),
+///             message_data_size: decode_u16(instr_data_slice, 10),
+///             message_instruction_index: decode_u16(instr_data_slice, 12),
+///         };
+///
+///         Ok(offsets)
+///     }
+/// }
+///
+/// const AUTHORIZED_ED25519_PUBKEY: [u8; PUBLIC_KEY_LENGTH] = [
+///     211, 210, 72, 176, 173, 140, 129, 224, 36, 99, 29, 4, 141, 117, 74, 94, 173, 213, 199, 210, 26,
+///     108, 206, 227, 55, 76, 126, 162, 14, 112, 100, 112,
+/// ];
+/// const EXPECTED_MESSAGE: &[u8] = b"This is a demo message.";
+///
+/// fn process_ed25519_instructions(
+///     instruction: CustomEd25519Instruction,
+///     accounts: &[AccountInfo],
+/// ) -> ProgramResult {
+///     use ed25519_defs::*;
+///     msg!("processing ed25519 instructions");
+///
+///     let account_info_iter = &mut accounts.iter();
+///
+///     // The instructions sysvar gives access to the instructions in the transaction.
+///     let instructions_sysvar_account = next_account_info(account_info_iter)?;
+///     assert!(sysvar::instructions::check_id(
+///         instructions_sysvar_account.key
+///     ));
+///
+///     let ed25519_instr = sysvar::instructions::load_instruction_at_checked(0, instructions_sysvar_account)?;
+///
+///     assert!(ed25519_program::check_id(&ed25519_instr.program_id));
+///     assert!(ed25519_instr.data.len() > 1);
+///
+///     let num_signatures = ed25519_instr.data[0];
+///     assert_eq!(1, num_signatures);
+///
+///     let offsets = ed25519_defs::load_signature_offsets(&ed25519_instr.data)?;
+///
+///     let pubkey_start = usize::from(offsets.public_key_offset);
+///     let pubkey_end = pubkey_start.saturating_add(PUBKEY_SERIALIZED_SIZE);
+///     let ed25519_instr_pubkey_slice = &ed25519_instr.data[pubkey_start..pubkey_end];
+///     let ed25519_instr_pubkey = ed25519_dalek::PublicKey::from_bytes(ed25519_instr_pubkey_slice)
+///         .map_err(|_| ProgramError::InvalidArgument)?;
+///
+///     if ed25519_instr_pubkey_slice != AUTHORIZED_ED25519_PUBKEY {
+///         return Err(ProgramError::InvalidArgument);
+///     }
+///
+///     let expected_message = EXPECTED_MESSAGE;
+///     assert_eq!(
+///         usize::from(offsets.message_data_size),
+///         expected_message.len()
+///     );
+///
+///     let msg_start = usize::from(offsets.message_data_offset);
+///     let msg_end = msg_start.saturating_add(usize::from(offsets.message_data_size));
+///     let ed25519_instr_message = &ed25519_instr.data[msg_start..msg_end];
+///     assert_eq!(
+///         ed25519_instr_message,
+///         expected_message,
+///     );
+///    
+///    Ok(())
+/// }
+/// #
+/// # use borsh::{BorshDeserialize, BorshSerialize};
+/// # use solana_program::pubkey::Pubkey;
+/// #
+/// # /// # Accounts
+/// # ///
+/// # /// - 0: instructions sysvar
+/// # #[derive(BorshSerialize, BorshDeserialize, Debug)]
+/// # pub struct CustomEd25519Instruction;
+/// #
+/// # let instruction = CustomEd25519Instruction {};
+/// # let p = Pubkey::new_unique();
+/// # let l = &mut 0;
+/// # let d = &mut [0u8];
+/// # let a = AccountInfo::new(&p, false, false, l, d, &p, false, 0);
+/// # let accounts = &[a];
+/// #
+/// # process_ed25519_instructions(instruction, accounts.as_slice());
+/// # Ok::<(), ProgramError>(())
 /// ```
 pub fn new_ed25519_instruction(keypair: &ed25519_dalek::Keypair, message: &[u8]) -> Instruction {
     let signature = keypair.sign(message).to_bytes();
